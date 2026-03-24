d735823bff1635459575d847c0d4cc20ec3572ed
AnhLe-dev <anhld2004@gmail.com>
2026-03-24 12:07:01 +0700

    Delete UrlShortener directory

--- UrlShortener/UrlShortener.Data/Entities/Identity/UrlShortenerRole.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace UrlShortener.Data.Entities.Identity
-{
-    public class UrlShortenerRole : IdentityRole
-    {
-        public string? Description { get; set; }
-    }
-}
